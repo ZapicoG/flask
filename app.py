ca5d35e5e7c85fe1c67182fcaab4f16be9d57709
Zapico Gonzalo <93474841+ZapicoG@users.noreply.github.com>
2023-03-12 22:04:33 +0000

    ttest

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ async def send_email_alert(subject, body):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
